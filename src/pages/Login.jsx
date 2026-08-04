a40b349e9dc14b38c7ea78881f7256e1c3faa87b
nana <comfortappiah.ca@gmail.com>
2026-08-04 22:14:56 +0000

    Update login page to successfully login users existing  in firebase storage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
-function Login() {
+const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      setErr(true);
+    }
+  };
+
   return (
-		<div className="formContainer">
-			<div className="formWrapper">
-				<span className="logo">Queen Chat</span>
-				<span className="title">Login</span>
-				<form>
-					<input type="email" placeholder="email" />
-					<input type="password" placeholder="password" />
-					<button>Sign in</button>
-					<span>Something went wrong</span>
-				</form>
-				<p>You don't have an account? Register</p>
-			</div>
-		</div>
-	);
-}
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Queens Chat</span>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="email" required />
+          <input type="password" placeholder="password" required />
+          <button>Sign in</button>
+          {err && <span>Something went wrong</span>}
+        </form>
+        <p>
+          You don't have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
