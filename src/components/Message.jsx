dba207f8383a5084612a705cbc11525ce675ecb5
nana <comfortappiah.ca@gmail.com>
2026-08-05 10:41:05 +0000

    Implement message component to work as expected

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-function Message() {
-	return (
-		<div class="message owner">
-			<div className="messageInfo">
-				<img
-					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOk-oWD03Zl6Jn4lkk1gFhZv5ckdi9dG4zNA&usqp=CAU"
-					alt=""
-				/>
-				<span>just now</span>
-			</div>
-			<div className="messageContent">
-				<p>hello</p>
-				 
-			</div>
-		</div>
-	);
-}
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
+  return (
+    <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
+      <div className="messageInfo">
+        <img
+          src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL}
+          alt="user avatar"
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="attached" />}
+      </div>
+    </div>
+  );
+};
 
 export default Message;
